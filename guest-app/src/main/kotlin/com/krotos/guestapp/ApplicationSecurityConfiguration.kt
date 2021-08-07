379e41cf3fc36c48be5848d942fb3f69ad64c680
Mariusz Krotos <mar.krotos@gmail.com>
2021-08-07 23:52:22 +0200

    In memory authentication

--- guest-app/src/main/kotlin/com/krotos/guestapp/ApplicationSecurityConfiguration.kt
+++ guest-app/src/main/kotlin/com/krotos/guestapp/ApplicationSecurityConfiguration.kt
@@ -1,9 +1,14 @@
 package com.krotos.guestapp
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 
 @Configuration
 @EnableWebSecurity
@@ -15,5 +20,14 @@ class ApplicationSecurityConfiguration: WebSecurityConfigurerAdapter() {
             .anyRequest().authenticated()
             .and()
             .httpBasic()
+    }
+
+    @Bean
+    override fun userDetailsService(): UserDetailsService {
+        val users = listOf<UserDetails>(
+            User.withDefaultPasswordEncoder().username("me").password("pass").roles("USER", "ADMIN").build(),
+            User.withDefaultPasswordEncoder().username("he").password("buka").roles("USER").build(),
+        )
+        return InMemoryUserDetailsManager(users)
     }
 }
